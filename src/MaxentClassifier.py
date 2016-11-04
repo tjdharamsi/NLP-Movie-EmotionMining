b8d3de9178c6c6bfd858aa0bba57d74e41b6f9ac
Siddharth Varia <varia.siddharth@gmail.com>
2016-11-04 14:17:23 -0400

    Adding maxent classifier and preprocess.py

---
+++ src/MaxentClassifier.py
@@ -0,0 +1,103 @@
+import sys
+from preprocess import InputInstance
+import cPickle
+from collections import defaultdict
+from scipy import sparse
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import cross_val_score
+#from pycorenlp import StanfordCoreNLP
+# import json
+#from collections import OrderedDict
+#from sklearn.feature_extraction.text import CountVectorizer
+#from nltk.translate.ibm_model import Counts
+#from numpy import array
+
+class MaxentClassifier:
+    
+    def __init__(self):
+        print 'init'
+        self.X_train = None
+        self.y = None
+        self.clf = None
+        
+    def createFeatureVectors(self, annData):
+        print 'createFeatureVectors'
+        annTokens = []
+        y_train = []
+        for ii in xrange(len(annData)):
+            #atxt = json.loads(annData[ii].atext)
+            tokens = []
+            for s in annData[ii].atext['sentences']:
+                tokens += [(t['word'].lower(), t['pos']) for t in s['tokens'] if t['pos'] in ('JJ', 'NN', 'NNS', 'NNP', 'NNPS', 'RB', 'RBR', 'RBS', 'VB', 'VBD', 'VBG', 'VBN')]
+            #print tokens
+            annTokens.append(tokens)
+            y_train.append(annData[ii].label)
+        #print len(annTokens)
+        ccounts = defaultdict(lambda: 0)
+        for atlst in annTokens:
+            for at in atlst:
+                ccounts[at] += 1
+
+        vlst = ccounts.keys()
+        vlst.sort(key=lambda tup: tup[0])
+        
+#         print 'vlst: '
+#         for v in vlst:
+#             print v
+        
+        vocabulary = defaultdict()
+        for ii in xrange(len(vlst)):
+            #print 'vlst[ii]', vlst[ii]
+            vocabulary[vlst[ii]] = ii
+        
+#         for k, v in vocabulary.items():
+#             print k, v
+            
+#         ccounts = OrderedDict()
+#         for atlst in annTokens:
+#             for at in atlst:
+#                 if at in ccounts:
+#                     ccounts[at] += 1
+#                 else:
+#                     ccounts[at] = 1        
+        
+        V = []
+        I = []
+        J = []
+        for ii in xrange(len(annTokens)):
+            tmpd = defaultdict(lambda: 0)
+            for at in annTokens[ii]:
+                tmpd[at] += 1
+            for key in tmpd:
+                V.append(tmpd[key])
+                I.append(ii)
+                J.append(vocabulary[key])
+        
+        X_train = sparse.coo_matrix((V,(I,J)),shape=(len(annTokens),len(vocabulary)))
+        labels = defaultdict()
+        for ii in xrange(len(y_train)):
+            labels[y_train[ii]] = ii    
+        y = [labels[y_i] for y_i in y_train]
+        
+        self.X_train = X_train
+        self.y = np.asarray(y)
+    
+    def train(self):
+        self.clf = LogisticRegression(solver='sag', max_iter=1000, random_state=42,
+                             multi_class='ovr')
+    def crossvalidate(self):
+        scores = cross_val_score(self.clf, self.X_train, self.y, cv=5)
+        print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))        
+        
+
+if __name__ == '__main__':
+    
+    annData = None
+    with open(sys.argv[1], 'rb') as f:
+        annData = cPickle.load(f)
+        
+    classifier = MaxentClassifier()
+    classifier.createFeatureVectors(annData)
+    classifier.train()
+    classifier.crossvalidate() 
---
+++ src/preprocess.py
@@ -0,0 +1,117 @@
+from pycorenlp import StanfordCoreNLP
+import sys, getopt
+import cPickle
+import json
+import re
+
+class InputInstance:
+    def __init__(self, i, ts, text):
+        self.i = i
+        self.ts = ts
+        self.text = text
+        self.atext = None        
+        self.label = None
+        
+    def __str__(self):
+        iidict = {}
+        iidict['i'] = self.i
+        iidict['ts'] = self.ts
+        iidict['text'] = self.text
+        iidict['atext'] = self.atext
+        iidict['label'] = self.label
+        return json.dumps(iidict)
+        
+class PreProcessor:
+    
+    def __init__(self):
+        print 'Initializing PreProcessor'
+
+    def annotateAll(self, inputData):
+        nlp = StanfordCoreNLP('http://localhost:9000')
+        for inp in inputData:
+            #inp.atext = json.dumps(nlp.annotate(inp.text, properties={'annotators': 'tokenize, ssplit, pos, lemma, ner','outputFormat': 'json'}))
+            inp.atext = nlp.annotate(inp.text, properties={'annotators': 'tokenize, ssplit, pos, lemma, ner','outputFormat': 'json'})
+            #print inp.atext
+            
+    def readInput(self, inputXFile, inputYFile):
+        inputData = []
+        with open(inputXFile,'r') as f:
+            currInput = []
+            for line in f:
+                line = line.strip()
+#                 print 'len(line): ', len(line)
+                if len(line) > 0:
+                    #print line
+                    #print line.encode('utf-8')
+                    line = re.sub(r'[^\x00-\x7F]+',' ', line)
+                    #print line
+                    currInput.append(line)
+                else:
+                    i = currInput[0]
+#                     print '###############'
+#                     print i
+#                     print '###############'                    
+                    ts = currInput[1]
+#                     print '###############'
+#                     print ts
+#                     print '###############'
+                    text = ' '.join(currInput[2:])
+                    #print text
+                    #text = re.sub(u'[^\u0000-\u007e]+',' ', text)
+                    #re.sub(r'[^\x00-\x7F]+',' ', text)
+#                     print '###############'
+#                     print text
+#                     print '###############'                    
+                    inputData.append(InputInstance(i, ts, text))
+                    currInput = []
+        inputData.append(InputInstance(currInput[0], currInput[1], ' '.join(currInput[2:])))
+        
+        with open(inputYFile, 'r') as f:
+            labels = f.readlines()
+            assert (len(labels) == len(inputData)),'number of labels not equal to number of inputs'
+            ii = 0
+            for l in labels:
+                inputData[ii].label = l.strip()
+                ii += 1
+        
+#         for idata in inputData:
+#             print idata
+            
+        return inputData
+
+    def preprocess(self, inputXFile, inputYFile):
+        print 'preprocessing data...'
+        inputData = self.readInput(inputXFile, inputYFile)
+        self.annotateAll(inputData)
+        return inputData
+
+if __name__ == '__main__':
+    inputXFile = None
+    inputYFile = None
+    dumpFile = None       
+    try:
+        options, remainder = getopt.getopt(sys.argv[1:], 'h', ['help', 'xfile=', 'yfile=', 'dfile='])
+    except getopt.GetoptError as err:
+        print(err)
+        print 'preprocess.py --xfile <x_file> --yfile <yte_file> --dfile <dump_file>'
+        sys.exit(2)
+    for opt, arg in options:
+        if opt in ('-h', '-help'):
+            print 'preprocess.py --xfile <x_file> --yfile <yte_file> --dfile <dump_file>'
+            sys.exit()
+        elif opt == '--xfile':
+            inputXFile = arg
+        elif opt == '--yfile':
+            inputYFile = arg
+        elif opt == '--dfile':
+            dumpFile = arg        
+        else:
+            print 'unhandled option'
+    
+    print 'inputXFile: ', inputXFile
+    print 'inputYFile: ', inputYFile
+    pp = PreProcessor()
+    ainputData = pp.preprocess(inputXFile, inputYFile)
+    if dumpFile is not None:
+        with open(dumpFile, 'wb') as fp:
+            cPickle.dump(ainputData, fp)
