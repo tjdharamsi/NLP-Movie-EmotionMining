bedfa5316b5831622a05a2760fbe71108ea757a5
dharamsitejas <tejas.dharamsi@gmail.com>
2016-12-03 18:42:39 -0500

    other altos

--- src2/MaxentClassifier.py
+++ src2/MaxentClassifier.py
@@ -7,6 +7,7 @@ import numpy as np
 from sklearn.linear_model import LogisticRegression
 from sklearn import svm
 from sklearn.cross_validation import cross_val_score
+from sklearn.ensemble import RandomForestClassifier
 #from pycorenlp import StanfordCoreNLP
 # import json
 #from collections import OrderedDict
@@ -36,7 +37,7 @@ class MaxentClassifier:
             y_train.append(annData[ii].label)
 
         #----------Removes EmotionLesss-------
-        """key=[]
+        key=[]
         for i in range(len(y_train)):
             if(y_train[i]=="emotionless"):
                 key.append(i)
@@ -49,7 +50,7 @@ class MaxentClassifier:
                 YT.append(y_train[i])
         
         annTokens=AnnT
-        y_train=YT"""
+        y_train=YT
 
         #-----------------------
 
@@ -109,10 +110,15 @@ class MaxentClassifier:
         self.y = np.asarray(y)
     
     def train(self):
-        self.clf = svm.LinearSVC( max_iter=1000, random_state=42,
-                             multi_class='ovr')
+        #RBF Kernel
+        self.clf = svm.SVC( kernel="rbf",max_iter=1000, random_state=42,decision_function_shape='ovr')
+        
+        #Linear SVC
+        #self.clf = svm.LinearSVC( max_iter=1000, random_state=42,multi_class='ovr')
+
+        #self.clf=RandomForestClassifier(n_estimators=10, criterion='gini', max_depth=None, min_samples_split=4, min_samples_leaf=1, min_weight_fraction_leaf=0.0, max_features='auto', max_leaf_nodes=None, bootstrap=True, oob_score=False, n_jobs=1, random_state=None, verbose=0, warm_start=False, class_weight=None)
     def crossvalidate(self):
-        scores = cross_val_score(self.clf, self.X_train, self.y, cv=5)
+        scores = cross_val_score(self.clf, self.X_train, self.y, cv=5,scoring="accuracy")
         print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))        
         
 
