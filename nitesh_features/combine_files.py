4b00a9198af4adf8886b8b78d5a95acde9fcc3cc
niteshsurtani <nitesh.surtani0606@gmail.com>
2016-12-06 03:23:42 -0500

    Adding combined files

---
+++ nitesh_features/combine_files.py
@@ -0,0 +1,21 @@
+episode = "combined"
+
+original_srt_file = episode + ".srt"
+fr = open(original_srt_file, "r")
+fw = open("new_combined.srt", "w")
+
+i = 1
+k = 0
+for line in fr:
+	if k == 0:
+		fw.write(str(i) + "\n")
+
+	else:
+		fw.write(line)
+
+	k += 1
+
+	if line.strip() == "":
+		k = 0
+		i += 1	
+
